5ebe9b5ebdaa7540925d406fd5fdfb3acbedff4b
Aidan <1aidanrj@gmail.com>
2025-05-13 12:05:53 +0100

    feat: handle POST request JSON parsing and chirp length handler

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerHealthz)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	server := http.Server{
 		Addr:    ":8080",
