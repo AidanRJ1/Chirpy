0b6d7b7ef82483e910602677b20ebd27da997e0d
Aidan <1aidanrj@gmail.com>
2025-05-13 11:14:51 +0100

    Feat: changed /api/ to /admin/ for some subpaths

--- metrics.go
+++ metrics.go
@@ -13,9 +13,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	body := fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())
+	body := fmt.Sprintf(`
+<html>
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %d times!</p>
+</body>
+</html>
+	`, cfg.fileServerHits.Load())
 
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(body))
 }
